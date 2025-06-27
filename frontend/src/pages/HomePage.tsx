e006e74ec9e5b7170c195412d8c91abc88ea3804
Ahmed Marzook <m4marzook@gmail.com>
2025-06-27 11:18:18 +0100

    feat: implement HomePage layout with product display and creation link

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,5 +1,37 @@
-import React from "react";
+import { Container, VStack, Text } from "@chakra-ui/react";
+import { Link } from "react-router-dom";
 
 export default function HomePage() {
-  return <div>HomePage</div>;
+  return (
+    <Container maxW="container.xl" py={12}>
+      <VStack>
+        <Text
+          fontSize={"30"}
+          fontWeight={"bold"}
+          bgGradient={"linear(to-r, cyan.400, blue.500)"}
+          bgClip={"text"}
+          textAlign={"center"}
+        >
+          Current Products 🚀
+        </Text>
+        <Text
+          fontSize={"xl"}
+          textAlign={"center"}
+          fontWeight={"bold"}
+          color={"gray.500"}
+        >
+          No Products found 😔
+          <Link to={"/create"}>
+            <Text
+              as="span"
+              color={"blue.500"}
+              _hover={{ textDecoration: "underline" }}
+            >
+              Create a product
+            </Text>
+          </Link>
+        </Text>
+      </VStack>
+    </Container>
+  );
 }
